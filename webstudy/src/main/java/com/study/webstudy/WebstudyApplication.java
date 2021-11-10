3bb063aa20052216a2f247bd39172a8aa39698a5
Y <igor2.004@mail.ru>
2021-11-11 00:03:02 +0300

    v1.0.0

---
+++ webstudy/src/main/java/com/study/webstudy/WebstudyApplication.java
@@ -0,0 +1,28 @@
+package com.study.webstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstudyApplication.class, args);
+	}
+
+}
+// https://getbootstrap.com/docs/5.1/examples/
+
+// https://start.spring.io/
+// #!type=maven-project
+// &language=java
+// &platformVersion=2.5.6
+// &packaging=jar
+// &jvmVersion=1.8
+// &groupId=com.study
+// &artifactId=webstudy
+// &name=webstudy
+// &description=coursework%20webservice
+// &packageName=com.study.webstudy
+// &dependencies=web,mysql,thymeleaf,devtools
+
---
+++ webstudy/src/main/java/com/study/webstudy/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.study.webstudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "main page");
+        return "home";
+    }
+
+}
